b29301f583d9349ad143855a479fea18c0b7dc1a
timgrafikon <timgrafikon@gmail.com>
2024-09-11 12:02:16 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+// about container
+ScrollReveal().reveal(".about__item", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+
+
+// blog container
+ScrollReveal().reveal(".blog__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach(counter => {
+      const updateCount = () => {
+          const target = +counter.getAttribute('data-target');
+          const count = +counter.innerText;
+          
+          // Determine the speed of increment
+          const increment = target / 200; // Adjust the number of steps to slow down or speed up the animation
+
+          if (count < target) {
+              counter.innerText = Math.ceil(count + increment);
+              setTimeout(updateCount, 25); // Adjust timeout to control the animation speed
+          } else {
+              counter.innerText = target;
+          }
+      };
+
+      updateCount();
+  });
+});
